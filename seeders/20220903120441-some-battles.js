477e953bfdd52042c1f5d38adf8cfa00a8963b57
arkhamz <shile2007@gmail.com>
2022-09-05 12:23:40 +0200

    zusmarshausen seed

--- seeders/20220903120441-some-battles.js
+++ seeders/20220903120441-some-battles.js
@@ -20,6 +20,7 @@ const transylvania = pathToFileURL("../assets/transylvania.png")
 const stralsund = pathToFileURL("../assets/stralsund.png")
 const scots = pathToFileURL("../assets/scots.png")
 const france = pathToFileURL("../assets/france.png")
+const jankauPic = pathToFileURL("../assets/jankau.jpeg")
 
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -258,7 +259,7 @@ module.exports = {
       },
       {
         // START OF BATTLE OBJECT
-        prelude: "A string of protestant defeats led France to intervene and declare war on the Habsburgs (Holy Roman Empire) and spanish empire on 19th May 1635, to limit Habsburg power. This was despite France being a Catholic power which suppressed its own Protestant rebellions. An initial invasion of the spanish netherlands ended in failure, witht the french retreating to their borders. A french defeat at the Battle of Honnecourt in 1642 opened the way to paris, spanish troops under Francisco de melo laid siege to the fortified town of Rocroi. 21 year old Louis de Bourbon, Prince of Condé, was appointed to stop him",
+        prelude: "A string of protestant defeats led France to intervene and declare war on the Habsburgs (Holy Roman Empire) and spanish empire on 19th May 1635, to limit Habsburg power. This was despite France being a Catholic power which suppressed its own Protestant rebellions. An initial invasion of the spanish netherlands ended in failure, witht the french retreating to their borders. The peace of prague in 1935 also saw most of the rebellious German protestant states exit the war. A french defeat at the Battle of Honnecourt in 1642 opened the way to paris, spanish troops under Francisco de melo laid siege to the fortified town of Rocroi. 21 year old Louis de Bourbon, Prince of Condé, was appointed to stop him",
         name: "Rocroi",
         description: "The battle began with a French cavalry attack which pushed back and scattered the opposing Spanish cavalry. An unauthorised french cavalry atack on the spanish was repulsed, with the spanish mounting a counteer attack which was later stopped by the french. Louis Conde's cavalry encircled the spanish and crashed through their rear, scattering the Spanish cavalry and artillery crews. The trapped and encircled spanish infantry held their ground and were pummeled with french artillery and captured spanish guns. Impressed with the bravery of the spanish troops, Conde offered the option of surrender which the spanish accepted. However upon riding towards them to accept their surrender, confused spanish troops opened fire on the prince, who was able to retreat. French forces responded with a devastating attack in whch the Spanish army was virtually destroyed.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Rocroi.jpg",
@@ -350,42 +351,42 @@ module.exports = {
       },
       {
         // START OF BATTLE OBJECT
-        prelude: "",
+        prelude: "With the 1635 Treaty of Prague, most of Sweden's protestant German allies made peace. The war lost much of its religious aspect, and turned into a contest between the Holy Roman Empire, and the Swedish Empire supported by France and others. The Danes also re-enetered the war as an imperial ally. THe swedes, led by Lennart Torstensson began plans for an offensive wich would force Ferdinand III, Holy Roman Emperor, to agree to peace terms. While France attacked Bavaria, Torstensson would move into Bohemia then strike at the Imperial capital at Vienna.",
         name: "Jankau",
-        description: "",
-        imageUrl: "",
-        result: "",
+        description: "The morning of the battle saw a swedish army column under scotsman Robert Douglas attempt a diversionary attack against the Imperial army's right. After leaving to assess this swedish move, Imperial commander Von Hatzfeldt found that his subordinate Von Gotzen, had misinterpreted his orders while he had gone, moving his entire force to a hill and getting stuck in the woods, allowing the swedes time to place artillery guns and infantry near them. After a furious argument, von Gotzen launched numerous attacks at the swedes, which were repulsed with heavy losses, until he himself was killed and his troops retreated. Hatzfeld held his position until nightfall, prompting Torstensson to move his artillery closer and fire at close range at the helpless Imperial infantry. Von Werth's Imperial cavalry charged and scattered the Swedish right, before stopping to loot the swedish baggage train, where they also captured Torstensson's wife. The swedish cavalry regrouped and counter-attacked the distracted Imperials, inflicting heavy casualties. The isolated Imperial infantry surrendered, many prisoners were taken, including Imperial commander Von Hatzfeldt.",
+        imageUrl: `${jankauPic}`,
+        result: "Swedish anti-imperial victory",
         year: "1645",
-        date: "",
+        date: "6 March 1645",
         latitude: "49.6503",
         longitude: "14.7294",
         army_one: JSON.stringify({
-          beligerents:[["",],["",],["", ]],
-          commanders: [["",],["",],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Holy Roman Empire",hre]],
+          commanders: [["Von Hatzfeldt",hre],["Von Werth",bavaria],["Von Gotzen",hre ]],
+          strength: {number: 16000, guns: 26},
+          casualties: "4,000 killed or wounded, 4,450 & 26 guns captured"
         }),
         army_two: JSON.stringify({
-          beligerents:[["",],["",]],
-          commanders: [["P", ],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Swedish Empire",swedish]],
+          commanders: [["Lennart Torstensson",swedish ],["Arvid Wittenberg",swedish ],["Mortaigne de Potelles",swedish ],["Robert Douglas",scots ]],
+          strength: {number: 16000, guns: 60},
+          casualties: "3,000-4,000 killed or wounded"
         }),
         questions: JSON.stringify({
           question1: {
-            text: "",
+            text: "What type of attack did scotsman Robert Douglas try to do?",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "A frontal charge", isCorrect:false},
+              {answerText: "A diversionary attack", isCorrect:true},
+              {answerText: "An artillery barrage", isCorrect:false},
             ]
           },
           question2: {
-            text: "?",
+            text: "What distracted Von Werth's imperial cavalry?",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "The swedish baggage train", isCorrect:true},
+              {answerText: "An unknown army", isCorrect:false},
+              {answerText: "A comet", isCorrect:false},
             ]
           },
          
@@ -396,42 +397,42 @@ module.exports = {
       },
       {
         // START OF BATTLE OBJECT
-        prelude: "",
+        prelude: " Upon reaching Kassel on July 1st, and managing to immediately insult his allied German commander Johann von Geyso, Louis II prince of conde and the allied army moved to interrupt the Bavarians led by Von Mercy, who were besieging the strategically significant Hessian stronghold of Kirchheim to block off the french army in the area. The french approach forced them to break the siege, both armies then raced towards Heilbronn, with Conde's force arriving first and taking WImpfen. Both armies continued to the east,with conde taking another city. Conde then turned south to threaten Bavaria, attempting to besiege DInkelsbuhl. Constant Bavarian cavalry raids forced Conde to attempt a direct attack on Von Mercy's bavarian army, but as the area was nt suited for a battle, the French troops marched towards Nordlingen, shadowed by von Mercy ",
         name: "Nordlingen",
-        description: "",
-        imageUrl: "",
-        result: "",
+        description: "An impatient Conde ordered the attack to start at 5PM, ignoring commander Turenne's advice to wait till dawn. The french center approached with their artillery guns towards the Bavarians, and were immediately devestated by artillery fire once in range. They were able to push through and capture a village, but were counter attacked by Mercy's men and artillery fire, ultimately causing them to route. A further attack with a smaller group, led by Conde himself, was shattered before reaching the enemy. By 7pm, most of the french center was destroyed. Von Mercy, on the verge of his greatest victory and ecstatic at the progress of the battle, was killed by a wayward cannonball. His second-in command failed to take control and destroy the french. The lack of coordination and overall strategy following Mercy's death led to french forces destroying the bavarian left and then pouncing on the disorganised center, triggering a general rout.",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Nordlingen.jpg/1024px-Nordlingen.jpg",
+        result: "French victory",
         year: "1645",
-        date: "",
+        date: "August 3 1645",
         latitude: "48.805556",
         longitude: "10.485833",
         army_one: JSON.stringify({
-          beligerents:[["",],["",],["", ]],
-          commanders: [["",],["",],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Holy Roman Empire",hre],["Electorate of Bavaria",bavaria]],
+          commanders: [["Franz von Mercy",bavaria],["Johann von Werth",hre]],
+          strength: {number: 16000, guns: 28},
+          casualties: "2,500 killed and wounded, 1,500 captured"
         }),
         army_two: JSON.stringify({
-          beligerents:[["",],["",]],
-          commanders: [["P", ],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Kingdom of France",france],["Hesse-Kassel",hesseKassel]],
+          commanders: [["Louis II de Conde", france ],["Vicomte de Turenne", france ],["Johann von Geyso",hesseKassel]],
+          strength: {number:17000, guns:27},
+          casualties: "4000 killed or wounded"
         }),
         questions: JSON.stringify({
           question1: {
-            text: "",
+            text: "What caused Conde to attack before the allied French army had planned?",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "Courage", isCorrect:false},
+              {answerText: "French fury", isCorrect:false},
+              {answerText: "Impatience", isCorrect:true},
             ]
           },
           question2: {
-            text: "?",
+            text: "The death of what commander sealed the fate of the battle",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "Louis II Conde", isCorrect:false},
+              {answerText: "Von Mercy", isCorrect:true},
+              {answerText: "Vicomte de Turenne", isCorrect:false},
             ]
           },
          
@@ -442,42 +443,42 @@ module.exports = {
       },
       {
         // START OF BATTLE OBJECT
-        prelude: "",
+        prelude: "By the late 1640s all the belligerents in the Thirty Years War were exhausted by three decades of brutal fighting. Delegates had convened in the Westphalian cities of Munster and Osnabruck to negotiate a peace treaty in 1646. While these peace talks were in progress, the opposing powers continued to jockey for position in order to improve their positions in the negotiations. Sweden was keen for a final decisive victory against the Habsburg monarchy in order to gain territory inside the Habsburg monarchy, and also to make the most of the war while it lasted by invading and plundering the rich Habsburg province of Bohemia, left relatively untouched by fighting so far",
         name: "Zusmarshausen",
-        description: "",
-        imageUrl: "",
-        result: "",
+        description: "The battle began at 7, when Montecuccoli's troops in Zusmarshausen came under attack from the Swedish vanguard. Montecuccoli held off the Swedish attacks for an hour before ordering his men to pull back eastward to the village of Herpfenried, where they mounted another stand. This time, however, a group of French cavalry managed to work their way round the southern side of Montecuccoli's position, threatening to cut him off from the rest of the Imperial-Bavarian army. Melander himself dashed back to rescue the rearguard, and in the resulting melee the general was shot in the chest and killed. Montecuccoli managed to extract his surviving men and rejoin Gronsfeld and the rest of the army. Under cover of darkness, Gronsfeld and the imperial forces retreated to Augsburg,,",
+        imageUrl: "https://i.pinimg.com/originals/eb/5d/2a/eb5d2a4c4ea50a59b442cc617dd3442e.jpg",
+        result: "Franco-Swedish victory",
         year: "1648",
-        date: "",
+        date: "17 May 1648",
         latitude: "48.4002",
         longitude: "10.5967",
         army_one: JSON.stringify({
-          beligerents:[["",],["",],["", ]],
-          commanders: [["",],["",],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Electorate of Bavaria",bavaria],["Holy Roman Empire",hre]],
+          commanders: [["Peter Melander von Holzappel",hre],["Raimondo Montecuccoli",hre],["Jost Maximilian von Bronckhorst-Gronsfeld",bavaria ]],
+          strength: {number: 15370, guns:0},
+          casualties: "1,897 killed or wounded, 6 guns captured"
         }),
         army_two: JSON.stringify({
-          beligerents:[["",],["",]],
-          commanders: [["P", ],["", ]],
-          strength: {},
-          casualties: ""
+          beligerents:[["Swedish Empire",swedish],["Kingdom of France",france]],
+          commanders: [["Vicomte de Turenne",france ],["Carl Gustaf Wrangel", swedish ]],
+          strength: {number: 22000, guns: 0},
+          casualties: "More than 500 killed or wounded"
         }),
         questions: JSON.stringify({
           question1: {
-            text: "",
+            text: "Why was sweden keen for a final decisive victory against the Habsburgs?",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "A better negotiation position at peace talks", isCorrect:false},
+              {answerText: "War booty", isCorrect:false},
+              {answerText: "All of the above", isCorrect:true},
             ]
           },
           question2: {
-            text: "?",
+            text: "Imperial commander Peter Melander von Holzappel was killed trying to do what?",
             answerOptions:[
-              {answerText: "", isCorrect:false},
-              {answerText: "", isCorrect:true},
-              {answerText: "", isCorrect:false},
+              {answerText: "Rescue his men", isCorrect:true},
+              {answerText: "Retreat from the battle field", isCorrect:false},
+              {answerText: "Switch sides to the french-swedish", isCorrect:false},
             ]
           },
          
