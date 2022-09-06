ed21537f5f5e1c3c158194f97008cb222c84c8c2
arkhamz <shile2007@gmail.com>
2022-09-06 12:19:06 +0200

    added cors middleware to index.jx

--- index.js
+++ index.js
@@ -2,29 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+//import cors
+const corsMiddleWare = require("cors");
+
 //import routers
 const battleRouter = require("./routers/battles");
 const commanderRouter = require("./routers/commanders");
 
-
 //register middleware body parsers/routers
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(battleRouter);
 app.use(commanderRouter);
+app.use(corsMiddleWare());
 
-
-app.listen(PORT, function(){
-    console.log(`Server listening on port ${PORT}`);
-})
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}`);
+});
