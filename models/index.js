d92722ffc0ba53b64d2861c27fa3a32067cb6e75
arkhamz <shile2007@gmail.com>
2022-09-16 14:21:58 +0200

    used database URL in sequelize index.js line 15

--- models/index.js
+++ models/index.js
@@ -12,7 +12,8 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.url, config);
+  // sequelize = new Sequelize(config.url, config);
+  sequelize = new Sequelize(process.env.DATABASE_URL, config)
 }
 
 fs
