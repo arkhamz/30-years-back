fb5f172e6a450cb8f986ca8519814d3974acf4f0
arkhamz <shile2007@gmail.com>
2022-09-19 00:06:10 +0200

    header info

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 //import cors
 const corsMiddleWare = require("cors");
 app.use(corsMiddleWare({
-  'allowedHeaders': ['Content-Type'], // headers that React is sending to the API
+  'allowedHeaders': ['Content-Type', "Authorization", "authorization"], // headers that React is sending to the API
   'exposedHeaders': ['Content-Type'], // headers that you are sending back to React
   'origin': '*',
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
