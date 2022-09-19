a13bfbc9f27c9dd428753d1a55daf6ec1c0d7ae4
arkhamz <shile2007@gmail.com>
2022-09-19 20:26:43 +0200

    reverted changes to cors header arrays, added regex to private key thing

--- middleware/fireMiddleware.js
+++ middleware/fireMiddleware.js
@@ -9,7 +9,7 @@ const app = fbAdmin.initializeApp({
     credential: fbAdmin.credential.cert({
         projectId: `${process.env.PROJECT_ID}`,
         clientEmail: `${process.env.CLIENT_EMAIL}`,
-        privateKey: `${process.env.PRIVATE_KEY}`
+        privateKey: `${process.env.PRIVATE_KEY?.replace(/\\n/g, '\n')}`
     })
 });
 // change
